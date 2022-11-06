d77ed22c3fd84ecf46e876df3e87738aae8228d1
zizifn <515868058@qq.com>
2022-11-06 22:22:18 +0800

    add prox2

--- edge-bypass/pages/api/proxy2.ts
+++ edge-bypass/pages/api/proxy2.ts
@@ -1,38 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import net, { NetConnectOpts, TcpNetConnectOpts } from "node:net";
+import type { Readable } from "node:stream";
+
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  console.log("start");
-  const headers = req.headers;
-  const serverAddress = headers["x-host"] as string;
-  const remotePort = headers["x-port"];
-  if (!serverAddress || !remotePort) {
-    res.status(200).json({ name: "proxy" });
-    return;
-  }
-  let datas: any[] = [];
-  req.on("data", (chunk) => {
-    try {
-      //console.log("To JSON : ",chunk.toJSON())
-      //console.log("To String : ",chunk.toString())
-      //console.log(chunk.isEncoding())
-      datas.push(chunk);
-    } catch (e) {
-      console.log("Data : ", e);
-    }
-  });
-  req.on("end", () => {
-    console.log(datas[0]);
-    res.status(200).json({ data: null, error: "Success" });
-  });
 
-  console.log("ddddd");
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const rawBody = buf.toString("utf8");
+
+    res.json({ rawBody });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
 }
