c9d03d548b7b7811812a0f973f547b4c8b8aec77
zizifn <515868058@qq.com>
2022-11-06 19:32:14 +0800

    update

--- edge-bypass/pages/api/hello.ts
+++ edge-bypass/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const json = await fetch("https://rickandmortyapi.com/api/character").then(
+    (res) => res.json()
+  );
+  res.status(200).json(json);
 }
