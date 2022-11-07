f5c92cb5566e28f14ab9debb1606e9c79ecb8545
zizifn <515868058@qq.com>
2022-11-08 01:41:14 +0800

    add edge

--- pages/api/edge.ts
+++ pages/api/edge.ts
@@ -1,38 +1,22 @@
-// export default async function handler(_) {
-//   const encoder = new TextEncoder();
-
-//   const customReadable = new ReadableStream({
-//     start(controller) {
-//       controller.enqueue(encoder.encode("Basic Streaming Test"));
-//       controller.close();
-//     },
-//   });
-
-//   return new Response(customReadable, {
-//     headers: { "Content-Type": "text/html; charset=utf-8" },
-//   });
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 
 const hello = (req: NextRequest) => {
   console.log("start");
   const encoder = new TextEncoder();
-  const readableStream = new ReadableStream({
-    index: 0,
-    start(controller) {
-      const interval = setInterval(() => {
-        controller.enqueue(encoder.encode(`client send ${this.index++}`));
-      }, 500);
-
-      setTimeout(() => {
-        clearInterval(interval);
-        controller.close();
-      }, 10_000);
-    },
-  } as UnderlyingSource & { index: number });
-
-  return new Response(readableStream, {
+  const decoder = new TextDecoder();
+  const readableStream2 = req.body?.pipeThrough(
+    new TransformStream({
+      transform(chunk, controller) {
+        console.log("[transform]", decoder.decode(chunk));
+        controller.enqueue(chunk);
+      },
+      flush(controller) {
+        console.log("[flush]");
+        controller.terminate();
+      },
+    })
+  );
+  return new Response(readableStream2, {
     headers: { "Content-Type": "text/html; charset=utf-8" },
   });
 };
